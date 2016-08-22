4fdec3f003bf80e318dceb2387eb7b6d51c2e032
garageinc <9-b-rinat@rambler.ru>
2016-08-22 18:12:36 +0300

    up

---
+++ app/components/graphs/graph-create.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from 'angular2/core';
+import {RouteParams, Route, Router} from 'angular2/router';
+
+import { Graph } from './../../models/graph';
+import { GraphService } from './../../services/graph.service';
+
+@Component({
+  selector: 'my-graph-create',
+  templateUrl: 'app/views/graphs/graph-create.component.html',
+  styleUrls: ['app/assets/css/graph-create.component.css']
+})
+
+export class GraphCreateComponent{
+
+  @Input() name: string;
+
+  constructor(
+      protected _graphService: GraphService,
+      protected _routeParams: RouteParams,
+      protected  _router:Router) {
+  }
+
+
+  onCreate(){
+    this._graphService.create( this.name).subscribe( (result) => {
+
+      console.log("Result: " + result)
+
+      if (result) {
+        this._router.navigate(['Dashboard']);
+      } else {
+        alert("Не удалось создать!")
+      }
+    });
+  }
+}
