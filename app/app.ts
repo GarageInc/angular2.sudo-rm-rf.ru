07ad7692ef0146c445b052f42de0365d8efc7516
GarageInc <9-b-rinat@rambler.ru>
2016-09-04 01:04:11 +0300

    up

--- app/app.ts
+++ app/app.ts
@@ -2,6 +2,16 @@ import { bootstrap }    from 'angular2/platform/browser';
 import { AppComponent } from './app.component';
 import {enableProdMode} from "angular2/core";
 
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {
+    LocationStrategy,
+    HashLocationStrategy
+} from 'angular2/router';
+
 import { HTTP_PROVIDERS } from 'angular2/http';
 
-bootstrap(AppComponent,[HTTP_PROVIDERS]);
+bootstrap(AppComponent,[
+    HTTP_PROVIDERS,
+    ROUTER_PROVIDERS,
+    {provide: LocationStrategy, useClass: HashLocationStrategy}
+]);
--- app/models/tabs/DataTab.js
+++ app/models/tabs/DataTab.js
@@ -1,4 +1,6 @@
-System.register([], function(exports_1) {
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var DataTab;
     return {
         setters:[],
@@ -7,7 +9,7 @@ System.register([], function(exports_1) {
                 function DataTab() {
                 }
                 return DataTab;
-            })();
+            }());
             exports_1("DataTab", DataTab);
         }
     }
